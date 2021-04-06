fcd6ad37546dfda918ff58ebb21d0feeb55abee8
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-06 12:12:52 +0900

    v-text v-html v-bind

--- js/index.js
+++ js/index.js
@@ -4,13 +4,31 @@ new Vue({
   el: '#app',
   // いかにいろんなオブジェクトを記載できる
   data: {
-    message: "HelloWorld"
+    message: "HelloWorld",
+    number: 3,
+    ok: true,
+    html: "<h1>h１です</h1>",
+    url: "https://google.com",
+    attribute: "href",
+    // 動的にurlを生成する方法 指定したい属性へnumberを入れてあげる オブジェクトにまとめらる
+    // urlTwitter: "https://twitter.com",
+    // number: 31
+    twitterObject: {
+      href: "https://twitter.com",
+      id: 31
+    }
+
   },
   // メソッド作成
   methods: {
     reverseMessage: function(){
       // this.messageはdataの中のmessageオブジェクトにアクセスする
       this.message = this.message.split('').reverse().join('')
+    },
+    sayHi: function(){
+      // 自分のオブジェクトを返したい場合thisを記載する（インスタンスないでデータを扱いたい場合）
+      this.message = "HelloVueJS";
+      return this.message;
     }
   }
 })
